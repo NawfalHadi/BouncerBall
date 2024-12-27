43c25af408f808cc38902f9f50a55b1f105aee50
NawfalHadi <nawfalhdi@gmail.com>
2024-12-28 04:02:35 +0800

    games : field done

--- main/pages/GamesSimulationPage.py
+++ main/pages/GamesSimulationPage.py
@@ -17,20 +17,20 @@ class GamesSimulationPage:
         self.init_field()
 
     def init_field(self):
-        self.left_field = Line((70, 1), (70, 1 + FIELD_HEIGHT), BLACK)
-        self.right_field = Line((70 + FIELD_WIDTH, 1), (70 + FIELD_WIDTH, 1 + FIELD_HEIGHT), BLACK)
-        self.top_field = Line((70, 1), (70 + FIELD_WIDTH, 1), BLACK)
-        self.bottom_field = Line((70, 1 + FIELD_HEIGHT), (70 + FIELD_WIDTH, 1 + FIELD_HEIGHT), BLACK)
+        self.left_field = Line((START_FIELD_WIDHT, START_FIELD_HEIGHT), (START_FIELD_WIDHT, START_FIELD_HEIGHT + FIELD_HEIGHT), BLACK)
+        self.right_field = Line((START_FIELD_WIDHT + FIELD_WIDTH, START_FIELD_HEIGHT), (START_FIELD_WIDHT + FIELD_WIDTH, START_FIELD_HEIGHT + FIELD_HEIGHT), BLACK)
+        self.top_field = Line((START_FIELD_WIDHT, START_FIELD_HEIGHT), (START_FIELD_WIDHT + FIELD_WIDTH, START_FIELD_HEIGHT), BLACK)
+        self.bottom_field = Line((START_FIELD_WIDHT, START_FIELD_HEIGHT + FIELD_HEIGHT), (START_FIELD_WIDHT + FIELD_WIDTH, START_FIELD_HEIGHT + FIELD_HEIGHT), BLACK)
 
-        self.top_gk_left_field = Line((70, 183), (70 + FIELD_GK_WIDTH, 183), RED)
-        self.bottom_gk_left_field = Line((70, 393), (70 + FIELD_GK_WIDTH, 393), RED)
-        self.gk_left_vertical = Line((70 + FIELD_GK_WIDTH, 183), (70 + FIELD_GK_WIDTH, 183 + FIELD_GK_HEIGHT), BLACK)
+        self.top_gk_left_field = Line((START_FIELD_GK_WIDHT, START_FIELD_GK_HEIGHT), (START_FIELD_GK_WIDHT + FIELD_GK_WIDTH, START_FIELD_GK_HEIGHT), RED)
+        self.bottom_gk_left_field = Line((START_FIELD_WIDHT, START_FIELD_GK_HEIGHT + FIELD_GK_HEIGHT), (START_FIELD_WIDHT + FIELD_GK_WIDTH, START_FIELD_GK_HEIGHT + FIELD_GK_HEIGHT), RED)
+        self.gk_left_vertical = Line((START_FIELD_WIDHT + FIELD_GK_WIDTH, START_FIELD_GK_HEIGHT), (START_FIELD_WIDHT + FIELD_GK_WIDTH, START_FIELD_GK_HEIGHT + FIELD_GK_HEIGHT), BLACK)
 
-        self.center_field = Line((70 + FIELD_WIDTH / 2, 1), (70 + FIELD_WIDTH / 2, 1 + FIELD_HEIGHT), BLACK)
+        self.center_field = Line((START_FIELD_WIDHT + FIELD_WIDTH // 2, START_FIELD_HEIGHT), (START_FIELD_WIDHT + FIELD_WIDTH // 2, START_FIELD_HEIGHT + FIELD_HEIGHT), BLACK)
 
-        self.top_gk_right_field = Line((70 + FIELD_WIDTH - FIELD_GK_WIDTH, 183), (70 + FIELD_WIDTH, 183), RED)
-        self.bottom_gk_right_field = Line((70 + FIELD_WIDTH - FIELD_GK_WIDTH, 393), (70 + FIELD_WIDTH, 393), RED)
-        self.gk_right_vertical = Line((70 + FIELD_WIDTH - FIELD_GK_WIDTH, 183), (70 + FIELD_WIDTH - FIELD_GK_WIDTH, 183 + FIELD_GK_HEIGHT), BLACK)
+        self.top_gk_right_field = Line((START_FIELD_WIDHT + FIELD_WIDTH - FIELD_GK_WIDTH, START_FIELD_GK_HEIGHT), (START_FIELD_WIDHT + FIELD_WIDTH, START_FIELD_GK_HEIGHT), RED)
+        self.bottom_gk_right_field = Line((START_FIELD_WIDHT + FIELD_WIDTH - FIELD_GK_WIDTH, START_FIELD_GK_HEIGHT + FIELD_GK_HEIGHT), (START_FIELD_WIDHT + FIELD_WIDTH, START_FIELD_GK_HEIGHT + FIELD_GK_HEIGHT), RED)
+        self.gk_right_vertical = Line((START_FIELD_WIDHT + FIELD_WIDTH - FIELD_GK_WIDTH, START_FIELD_GK_HEIGHT), (START_FIELD_WIDHT + FIELD_WIDTH - FIELD_GK_WIDTH, START_FIELD_GK_HEIGHT + FIELD_GK_HEIGHT), BLACK)
 
 
     def draw_field(self):
@@ -44,11 +44,11 @@ class GamesSimulationPage:
         self.gk_left_vertical.draw(self.screen)
 
         self.center_field.draw(self.screen)
-        self.center_circle = pygame.draw.circle(self.screen, BLACK, (70 + FIELD_WIDTH / 2, 1 + FIELD_HEIGHT / 2), 73, 2)
-
+        self.center_circle = pygame.draw.circle(self.screen, BLACK, (START_FIELD_WIDHT + FIELD_WIDTH / 2, START_FIELD_HEIGHT + FIELD_HEIGHT / 2), 73, 2)
+        
         self.top_gk_right_field.draw(self.screen)
         self.bottom_gk_right_field.draw(self.screen)
-        self.gk_right_vertical.draw(self.screen)  
+        self.gk_right_vertical.draw(self.screen)
 
     def run(self):
         while self.isRunning:
@@ -59,6 +59,7 @@ class GamesSimulationPage:
                     self.isRunning = False
 
             self.draw_field()
+            # print(START_FIELD_GK_HEIGHT)
 
             pygame.display.update()
 
